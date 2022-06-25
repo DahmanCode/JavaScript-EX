bb3419058b58b741fee9432799ebe49054099178
DahmanCode <elaammariabderrahman@gmail.com>
2022-06-25 23:45:44 +0100

    If Condition

--- elzero/[31]TO[39]/Comparison & Logical Operators.js
+++ elzero/[31]TO[39]/Comparison & Logical Operators.js
@@ -30,3 +30,5 @@ let c = 10;
 console.log(a > b && a == c || a < b); // true
 console.log(a < b && a > c); // true
 console.log(!(a > b) && !(a > b) && !(a == c) && !(a < c)); // true
+
+
---
+++ elzero/[31]TO[39]/IfCondition.js
@@ -0,0 +1,134 @@
+// Assignment 1 :
+
+let num = 9; // "009"
+if (num < 10) {
+    num = "00" + num;
+}
+else if (num >= 10 && num < 100) {
+    num = "0" + num;
+}
+else {
+    num == num;
+}
+
+console.log(num);
+
+// Assignment 2:
+
+let Num1 = 9;
+let str = "9";
+let str2 = "20";
+
+if (Num1 == str) {
+    console.log("{num1} Is The Same Value As {str}");
+}
+
+if (Num1 == str && typeof Num1 != typeof str) {
+    console.log("{num1} Is The Same Value As {str} But Not The Same Type");
+}
+
+if (Num1 !== str2) {
+    console.log("{num1} Is Not The Same Value Or The Same Type As {str2}");
+}
+
+if (str != str2 && typeof str == typeof str) {
+    console.log("{str} Is The Same Type As {str2} But Not The Same Value" );
+}
+
+// Output
+/* 
+    "{num1} Is The Same Value As {str}"
+    "{num1} Is The Same Value As {str} But Not The Same Type"
+    "{num1} Is Not The Same Value Or The Same Type As {str2}"
+    "{str} Is The Same Type As {str2} But Not The Same Value" 
+*/
+
+// Assignment 3:
+
+let nuM1 = 10;
+let nuM2 = 30;
+let nuM3 = "30";
+
+nuM3 > nuM1 && typeof nuM3 != typeof nuM2 
+? console.log(`${nuM3} Is Larger Than ${nuM1} And Type string Not The Same Type As number`) 
+: nuM3 > nuM1 && nuM3 == nuM2 && typeof nuM3 != typeof nuM2 
+? console.log(`${nuM3} Is Larger Than ${nuM1} And Value Is The Same As ${nuM2} And Type string Not The Same Type As number`) 
+: nuM3 !== nuM1 && typeof nuM3 != typeof nuM2 
+? "{nuM3} Value And Type Is Not The Same As {nuM1} And Type Is Not The Same As {nuM2}" 
+: console.log(Unknown);
+
+// Needed Output
+/* 
+"30 Is Larger Than 10 And Type string Not The Same Type As number"
+"30 Is Larger Than 10 And Value Is The Same As 30 And Type string Not The Same Type As number"
+"{nuM3} Value And Type Is Not The Same As {nuM1} And Type Is Not The Same As {nuM2}"
+*/
+
+// Assignment 4:
+
+let num0 = 10;
+let nUm2 = 10;
+let num3 = "10";
+let num4 = 20;
+
+/*
+  Do Not Edit Below This Line
+  Needed Output
+  True 7 Times
+*/
+
+// Condition 1
+
+if (num0 > nUm2) {
+console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 2
+
+if (num0 > nUm2 && num0 < num4) {
+console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 3
+
+if (num0 > nUm2 && num0 === num3) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 4
+
+if ((num0 + nUm2) < num4) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 5
+
+if ((num0 + num3) < num4) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 6
+
+if ((num0 + nUm2 + num3) < num4) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 7
+
+if (num4 - (num0 + num3) + nUm2 === 21) {
+  console.log("True");
+} else {
+  console.log("False");
+}
