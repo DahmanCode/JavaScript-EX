796a537ef6762f442c3641e12081d70fa46e622f
DahmanCode <elaammariabderrahman@gmail.com>
2022-06-26 13:48:22 +0100

    Nested If Condition

--- elzero/[31]TO[39]/IfCondition.js
+++ elzero/[31]TO[39]/IfCondition.js
@@ -37,10 +37,10 @@ if (str != str2 && typeof str == typeof str) {
 
 // Output
 /* 
-    "{num1} Is The Same Value As {str}"
-    "{num1} Is The Same Value As {str} But Not The Same Type"
-    "{num1} Is Not The Same Value Or The Same Type As {str2}"
-    "{str} Is The Same Type As {str2} But Not The Same Value" 
+  "{num1} Is The Same Value As {str}"
+  "{num1} Is The Same Value As {str} But Not The Same Type"
+  "{num1} Is Not The Same Value Or The Same Type As {str2}"
+  "{str} Is The Same Type As {str2} But Not The Same Value" 
 */
 
 // Assignment 3:
@@ -49,13 +49,25 @@ let nuM1 = 10;
 let nuM2 = 30;
 let nuM3 = "30";
 
-nuM3 > nuM1 && typeof nuM3 != typeof nuM2 
-? console.log(`${nuM3} Is Larger Than ${nuM1} And Type string Not The Same Type As number`) 
-: nuM3 > nuM1 && nuM3 == nuM2 && typeof nuM3 != typeof nuM2 
-? console.log(`${nuM3} Is Larger Than ${nuM1} And Value Is The Same As ${nuM2} And Type string Not The Same Type As number`) 
-: nuM3 !== nuM1 && typeof nuM3 != typeof nuM2 
-? "{nuM3} Value And Type Is Not The Same As {nuM1} And Type Is Not The Same As {nuM2}" 
-: console.log(Unknown);
+/* nuM3 > nuM1 && typeof nuM3 != typeof nuM2 
+? console.log(`${nuM3} Is Larger Than ${nuM1} And Type string Not The Same Type As number
+${nuM3 == nuM2 
+  ? console.log(`${nuM3} Is Larger Than ${nuM1} And Value Is The Same As ${nuM2} And Type string Not The Same Type As number`) 
+  : console.log(Unknown)}
+  ${nuM3 !== nuM1 
+    ? console.log("{nuM3} Value And Type Is Not The Same As {nuM1} And Type Is Not The Same As {nuM2}") 
+    : console.log(Unknown)}`) 
+: console.log(Unknown); */
+
+if (nuM3 > nuM1 && typeof nuM3 != typeof nuM2) {
+  console.log(`${nuM3} Is Larger Than ${nuM1} And Type string Not The Same Type As number`);
+  if (nuM3 == nuM2 ) {
+    console.log(`${nuM3} Is Larger Than ${nuM1} And Value Is The Same As ${nuM2} And Type string Not The Same Type As number`);
+    if (nuM3 !== nuM1) {
+      console.log("{nuM3} Value And Type Is Not The Same As {nuM1} And Type Is Not The Same As {nuM2}");
+    }
+  }
+}
 
 // Needed Output
 /* 
@@ -66,10 +78,17 @@ nuM3 > nuM1 && typeof nuM3 != typeof nuM2
 
 // Assignment 4:
 
-let num0 = 10;
-let nUm2 = 10;
+/*
+let num1 = 10;
+let num2 = 10;
 let num3 = "10";
 let num4 = 20;
+*/
+
+let num0 = 19;
+let nUm2 = 10;
+let num3 = 19;
+let num4 = 49;
 
 /*
   Do Not Edit Below This Line
