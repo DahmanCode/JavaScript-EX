32ff8f2b160cb2057f3b8663f9c829e7185fa989
DahmanCode <elaammariabderrahman@gmail.com>
2022-07-06 23:59:41 +0100

    Ultimate Practice

--- elzero/Function/main.js
+++ elzero/Function/main.js
@@ -1,2 +1,23 @@
+/* 
+  Function Practice
+    -Parameters
+    -Default
+    -Rest
+    -Loop
+    -Condition
+*/
 
+function showInfo(User = "Un", Age = "Un", Rate = 0, Show = "Yes", ...Skills) {
+  document.write(`<div>`);
+  document.write(`<h2>Welcome, ${User}</h2>`);
+  document.write(`<p>Age: ${Age}</p>`);
+  document.write(`<p>Hour Rate: $${Rate}</p>`);
+  if (Show === "Yes") {
+    document.write(`<p>Skills: ${Skills.join(" | ")}</p>`);
+  } else {
+    document.write(`<p>Skills: No Skills</p>`);
+  }
+  document.write(`</div>`);
+}
 
+showInfo("Dahman", 22, 30, "Yes", "HTML", "CSS", "JavaScript");
