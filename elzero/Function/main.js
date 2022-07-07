bf8bc420de8f876b2f554b05843635884ee70109
DahmanCode <elaammariabderrahman@gmail.com>
2022-07-07 12:59:08 +0100

    Random Arguments Function Challenge

--- elzero/Function/main.js
+++ elzero/Function/main.js
@@ -29,3 +29,58 @@ function showInfo(User = "Un", Age = "Un", Rate = 0, Show = "Yes", ...Skills) {
 }
 
 showInfo("Dahman", 22, 30, "Yes", "HTML", "CSS", "JavaScript");
+
+
+/* ===== Random Arguments Function Challenge ===== */
+
+/*
+  ====================================
+  Create Function showDetails
+  Function Accept 3 Parameters [a, b, c]
+  Data Types For Info Is
+  - String => Name
+  - Number => Age
+  - Boolean => Status
+  Argument Is Random
+  Data Is Not Sorted Output Depend On Data Types
+  - Use Ternary Conditional Operator
+*/
+
+document.write(`=========================`);
+
+function showDetails(...a) {
+
+  let name, age, bl
+  
+  for (i = 0; i < a.length; i++){
+
+    typeof a[i] === "string" ? name = a[i]
+    : typeof a[i] === "number" ? age = a[i]
+    : typeof a[i] === "boolean" ? boolean()
+    : document.write(` Unkown,`)
+  }
+
+  function boolean() {
+
+    a[i] === true ? bl = `You Are Available For Hire`
+    : bl = `You Are Not Available For Hire`
+  }
+  
+  document.write(`<div>`);
+  document.write(`<p>`);
+
+  document.write(`Hello ${name}, Your Age Is ${age} ${bl}`);
+
+  document.write(`<p>`);
+  document.write(`</div>`);
+
+}
+
+showDetails("Osama", 38, true); 
+// "Hello Osama, Your Age Is 38, You Are Available For Hire"
+showDetails(38, "Osama", true); 
+// "Hello Osama, Your Age Is 38, You Are Available For Hire"
+showDetails(true, 38, "Osama"); 
+// "Hello Osama, Your Age Is 38, You Are Available For Hire"
+showDetails(false, "Osama", 38); 
+// "Hello Osama, Your Age Is 38, You Are Not Available For Hire"
