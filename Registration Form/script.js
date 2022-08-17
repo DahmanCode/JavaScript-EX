6232c410c0588fc19591e5cf7471b079a358013a
DahmanCode <elaammariabderrahman@gmail.com>
2022-08-17 12:03:23 +0100

    Add A Required Validation

--- Registration Form/script.js
+++ Registration Form/script.js
@@ -10,3 +10,28 @@ document.getElementById('password')
   .insertAdjacentHTML('beforebegin','<lable for="password">Password: </lable>')
 document.getElementById('confirmPassword')
   .insertAdjacentHTML('beforebegin','<lable for="confirmPassword">Confirm Password: </lable>')
+
+  /* 
+  Exercise 02
+  -----------
+  Add a required validation to each input that shows an error message next to the  entry if it does not have any text entered.
+*/
+
+const checkInput = (event) => {
+  const errorElement = Array.from(event.target.parentNode.querySelectorAll('span'));
+  if (event.target.value ==='' && !errorElement.length) {
+    event.target.insertAdjacentHTML('afterEnd', '<span class="text-danger">Required</span>')
+  }
+
+  if(errorElement && event.target.value!== '') {
+    errorElement.forEach(elem => elem.remove());
+  }
+};
+
+document.getElementById('username')
+  .addEventListener('blur', checkInput);
+document.getElementById('password')
+  .addEventListener('blur', checkInput);
+document.getElementById('confirmPassword')
+  .addEventListener('blur', checkInput);
+
